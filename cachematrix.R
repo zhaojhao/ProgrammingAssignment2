6a86926f3083dc73b2502eb757d79726f64ca1a1
Zhao Hao <zhao.hao@sbcglobal.net>
2014-10-21 16:20:09 -0700

    2nd edited Oct 21, 2014 by zhaojhao

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,17 @@
-## Put comments here that give an overall description of what your
-## functions do
+## How to test:
+##>  mat <- matrix(runif(9), nrow=3)   # generate a random matrix
+##>  cas <- makeCacheMatrix(mat)       # generate cache
+##>  cacheSolve(cas)                   # solve matrix from the cache
+## 
+## The result from the first run should be a matrix which is the inverse of 'mat'.
+##
+##>  cacheSolve(cas)
+##
+## The result from the second run should contain "getting cached data" before 
+## the inverse of 'mat' returned.
+##
 
-## Write a short comment describing this function
-
+## This function creates an object that can cache the inverse of an input matrix.
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y) {
@@ -18,10 +27,13 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This function retrieve the created inverse of the matrix set up by
+# the makeCacheMatrix function. If the inverse has already been calculated
+# then the cachesolve should retrieve the inverse from the cache. Otherwise
+# solve the inverse of the matrix and store it into the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Return a matrix that is the inverse of 'x'
   m <- x$getinverse()
   if(!is.null(m)) {
     message("getting cached data")
